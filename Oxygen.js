cd407cd4ec9f64af916337cc8a6844e99ad864f4
thilakchelliah <thilak.chelliah49@gmail.com>
2015-03-11 07:09:20 +0000

    commit message

--- Oxygen.js
+++ Oxygen.js
@@ -1,1 +1,12 @@
-hello
+/*
+ * **********************************************************
+ * OxygenJS v1.0
+ * Copyright (C) 2015  Thilak49(thilak.chelliah49@gmail.com)
+ * Licensed under http://www.apache.org/licenses/LICENSE-2.0
+ * **********************************************************
+ */
+
+Oxygenjs=function(){
+var _Alphabets='a,b,c,d,e,f,g,h,i,j,k,l,m,n,i,o,p,q,r,s,t,u,v,w,x,y,z',
+    _MonthName='Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec';
+}
